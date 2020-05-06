851161b257eff350aa6b898e8fb4a1f10d76a89b
Sayanto Roy <sroy.sayanto@gmail.com>
2020-05-07 00:58:45 +0530

    Commit

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
+
 const app = express();
+const bookRoute = express.Router();
 
-let port = process.env.PORT || 3000;
+bookRoute.route('/books')
+.get((req, res) => {
+  const response = [{ Hello : "Kaise Ho BRO>??" , Nameste : "How Are YOu BRO?>?"}
+                    , { Hello : "Kaise Ho BRO>??" , Nameste : "How Are YOu BRO?>?"}];
+  res.json(response);
+});
 
-app.get('/' ,(req ,res)=> {
+app.use('/api',bookRoute);
+
+
+const port = process.env.PORT || 3000;
+
+app.get('/' , (req , res) => {
     res.send("Welcome to the API first");
 });
 
-app.listen(port , ()=>{
+app.listen(port , () => {
     console.log(`We are listening on ${port}`);
-})
+});
+
